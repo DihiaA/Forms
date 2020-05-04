25723b1fe40390a353a8be1acdaf2b87e8d935a6
DihiaA <Nydia.didia@gmail.com>
2020-05-04 15:30:42 +0200

    First-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+import { PostsComponent } from './posts/posts.component';
+
+
+const appRoutes : Routes = [
+  {
+    path : 'contact-form' , 
+    component : ContactFormComponent 
+  },
+  {
+    path : 'posts',
+    component : PostsComponent
+  }
+ 
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent implements OnInit {
+
+  contactMethod = [
+    {id : 1 ,  name : 'Dihia'},
+    {id : 2 , name : 'Nydia'}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  log(x){
+    console.log(x);
+  }
+
+  onSubmit(x){
+    console.log(x);
+  }
+
+}
